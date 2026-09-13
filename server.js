749298f76e0e766bb8e4e38a88342d45a9adde1d
ha7qqnnd-debug <ha7qqnnd@gmail.com>
2026-09-13 18:47:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,22 @@ const net = require('net');
 const PORT = process.env.PORT || 8080;
 const wss = new WebSocket.Server({ port: PORT });
 
+const WALLET = "DRXavkypCRMbk4cZA9qLSdMZvxPqF52YVu";
+const WORKER = "yoy";
+
 wss.on('connection', (ws) => {
-    const stratum = net.connect(7019, 'minotaurx.na.mine.zpool.ca', () => {});
+    const stratum = net.connect(7019, 'minotaurx.na.mine.zpool.ca', () => {
+        const sub = JSON.stringify({ id: 1, method: "mining.subscribe", params: ["yoy-agent"] }) + '\n';
+        const auth = JSON.stringify({ id: 2, method: "mining.authorize", params: [WALLET + "." + WORKER, "c=DOGE"] }) + '\n';
+        
+        stratum.write(sub);
+        stratum.write(auth);
+    });
 
     ws.on('message', (msg) => stratum.write(msg + '\n'));
-    stratum.on('data', (data) => ws.send(data.toString()));
+    stratum.on('data', (data) => {
+        if (ws.readyState === WebSocket.OPEN) ws.send(data.toString());
+    });
 
     ws.on('close', () => stratum.destroy());
     stratum.on('close', () => ws.close());
